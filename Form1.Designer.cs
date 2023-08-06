ec24b63826fb4e4134c91091e1108d418e44844c
Alex <licoms1997@gmail.com>
2023-08-06 16:01:24 +0300

    Add Prediction button click

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.bPredict = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.bPredict);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(370, 141);
+            this.splitContainer1.SplitterDistance = 70;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(366, 66);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // bPredict
+            // 
+            this.bPredict.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bPredict.Location = new System.Drawing.Point(0, 0);
+            this.bPredict.Name = "bPredict";
+            this.bPredict.Size = new System.Drawing.Size(366, 63);
+            this.bPredict.TabIndex = 0;
+            this.bPredict.Text = "Predict";
+            this.bPredict.UseVisualStyleBackColor = true;
+            this.bPredict.Click += new System.EventHandler(this.bPredict_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(370, 141);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button bPredict;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace UltimatePredictor
 {
@@ -14,7 +15,40 @@ namespace UltimatePredictor
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();  
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e) { }
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e) { }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        // To get around the progressive animation, we need to move the
+                        // progress bar backwards.
+                        if (i == progressBar1.Maximum)
+                        {
+                            // Special case as value can't be set greater than Maximum
+                            progressBar1.Maximum = i + 1;   // Temporarily increase Maximum
+                            progressBar1.Value = i + 1;     // Move past
+                            progressBar1.Minimum = i;       // Temporarily increase Maximum
+                        }
+                        else
+                        {
+                            progressBar1.Value = i + 1;     // Move past
+                        }
+                        progressBar1.Value = i;             // Move to correct value
+                    }));
+                    Thread.Sleep(20);
+                }
+            });
+            MessageBox.Show("Prediction");
+          
         }
     }
 }
